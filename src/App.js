900690c2dc0382000bb0b30e16bab15c002d6ccf
jcano <jcanofuentes@gmail.com>
2023-05-16 11:34:42 +0200

    Refactored shader file structure and updated components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+
 import ThreeScene from './ThreeScene';
-import ThreeBasicScene from './ThreeBasicScene';
+import ThreeSceneStandardPipeline from './ThreeSceneStandardPipeline';
+import ThreeSceneCustomPipeline from './ThreeSceneCustomPipeline';
+
+
 function App() {
   return (
     <div className="App">
-      <ThreeScene />
+      <ThreeSceneCustomPipeline
+        vertexShaderFile='/assets/shaders/vt_simple.glsl'
+        fragmentShaderFile='/assets/shaders/fr_red.glsl'
+      />
     </div>
+
   );
 }
 
--- src/ThreeScene.js
+++ src/ThreeScene.js
@@ -1,165 +1,34 @@
-import Joystick from './Joystick';
-import React from "react";
-import * as THREE from "three";
+import React from 'react';
+import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import * as dat from 'dat.gui';
 
-class ThreeCanvas extends React.Component {
+class Scene extends React.Component {
     constructor(props) {
         super(props);
         console.log("Creating ThreeCanvas instance (inheriting from React.Component.)");
-        this.loadManager = new THREE.LoadingManager();
-        this.loadManager.onLoad = this.handleAllResourcesLoaded.bind(this);
-        this.updateDimensions = this.updateDimensions.bind(this);
     }
     // ------------------------------------------------------------------------------
     // Custom methods for handling the Three.js scene:
     // ------------------------------------------------------------------------------
-    handleAllResourcesLoaded() {
-        console.log('All resources loaded succesfully!');
-        this.setupScene();
-    }
-    loadTextures() {
-        console.log("Loading textures...");
-        this.textureLoader = new THREE.TextureLoader(this.loadManager);
-        this.diffuseMap = this.textureLoader.load('/assets/PS_Albedo_1024.png');
-        this.diffuseMap.colorSpace = THREE.SRGBColorSpace;
-        this.normalMap = this.textureLoader.load('/assets/PS_Normal_1024.png');
-    }
-    loadShaders() {
-        this.vertexShaderLoader = new THREE.FileLoader(this.loadManager);
-        this.fragmentShaderLoader = new THREE.FileLoader(this.loadManager);
-        this.vertexShaderLoader.load('/assets/vertex.glsl', (data) => {
-            console.log("Vertex shader loaded!");
-            this.vertexShader = data;
-        });
-        this.fragmentShaderLoader.load('/assets/fragment.glsl', (data) => {
-            console.log("Fragment shader loaded!");
-            this.fragmentShader = data;
-        });
-    }
-    createSceneAndRenderer() {
+    setupScene() {
+        console.log('Scene setup...');
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.setClearColor(0x222222);
-
         this.scene = new THREE.Scene();
-    }
-    createPerspectiveCamera() {
-        var fieldOfView = 60.0;
-        var aspectRatio = window.innerWidth / window.innerHeight;
-        var nearClippingPlane = 0.1;
-        var farClippingPlane = 1000.0;
-        var cameraDistance = 7.0;
-        this.camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearClippingPlane, farClippingPlane);
-        this.camera.position.set(0, 0, cameraDistance);
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.set(0, 10, 0);
         this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-
-    }
-    createCameraOrbitControls() {
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        //this.controls.minDistance = 1;
-        //this.controls.maxDistance = 10;
-        //this.controls.enablePan = false;
-        //this.controls.enableDamping = true;
-    }
-    createGeometry() {
-        const plane_material = new THREE.MeshPhongMaterial(
-            {
-                map: this.diffuseMap,
-                normalMap: this.normalMap,
-                wireframe: false
-            }
-        );
-        var plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(10, 10, 1, 1),
-            plane_material
-        );
-        this.scene.add(plane);
-    }
-    createLights() {
-        this.lightsGroup = new THREE.Group();
-        this.scene.add(this.lightsGroup);
-
-        this.pointLight = new THREE.PointLight(0xffffff, 1, 32, 1.1);
-        this.pointLight.position.set(0, 0, 5.25);
-        this.pointLightHelper = new THREE.PointLightHelper(this.pointLight, 0.125);
-        this.scene.add(this.pointLightHelper);
-        this.lightsGroup.add(this.pointLight);
-
-        this.dirLight = new THREE.DirectionalLight(0xffffff, 1);
-        this.dirLight.position.set(0, 0, 5.25);
-        this.dirHelper = new THREE.DirectionalLightHelper(this.dirLight, 0.5);
-        this.scene.add( this.dirHelper);
-        this.lightsGroup.add(this.dirLight);
-
-        this.dirLight.visible = false;
-        this.dirHelper.visible = false;
-        this.pointLight.visible = true;
-        this.pointLightHelper.visible = true;
-    }
-
-    createHelpers() {
-        const axesHelper = new THREE.AxesHelper(1);
-        this.scene.add(axesHelper);
-    }
-    setupScene() {
-        console.log('Scene setup...');
-
-        THREE.ColorManagement.enabled = true;
-        this.createSceneAndRenderer();
-        //this.createHelpers();
-        this.createPerspectiveCamera();
-        this.createCameraOrbitControls();
-        this.createGeometry();
-
-        this.createLights();
-
-        const gui = new dat.GUI();
-        this.lightParameters = {
-            azimuth: 0.0,
-            distance: 0.0
-        };
-        const guiLightsGroup = gui.addFolder('Lighting');
-        guiLightsGroup.add(this.lightParameters, 'azimuth', -Math.PI, Math.PI);
-        guiLightsGroup.add(this.lightParameters, 'distance', -Math.PI, Math.PI);
-
-        const options = {
-            toggle: false,
-        };
-
-        // Choose between directional o point lights
-        guiLightsGroup.add(options, 'toggle').name('Point / Directional').onChange((value) => {
-            if (value == true) {
-                this.dirLight.visible = true;
-                this.dirHelper.visible = true;
-                this.pointLight.visible = false;
-                this.pointLightHelper.visible = false;
-            } else {
-                this.dirLight.visible = false;
-                this.dirHelper.visible = false;
-                this.pointLight.visible = true;
-                this.pointLightHelper.visible = true;
-            }
-        });
-
+        this.scene.add(new THREE.AxesHelper(1));
+        this.scene.add( new THREE.GridHelper() )
         this.mount.appendChild(this.renderer.domElement);
         this.animate();
     }
-    updateDimensions() {
-        if (this.mount !== null) {
-            this.renderer.setSize(this.mount.clientWidth, this.mount.clientHeight);
-            this.camera.aspect = this.mount.clientWidth / this.mount.clientHeight;
-            this.camera.updateProjectionMatrix();
-            this.renderer.render(this.scene, this.camera);
-        }
-    }
+
     animate() {
         this.frameId = requestAnimationFrame(this.animate.bind(this));
         this.controls.update();
-        this.lightsGroup.rotation.x = this.lightParameters.distance;
-        this.lightsGroup.rotation.y = this.lightParameters.azimuth;
         this.renderer.render(this.scene, this.camera);
     }
 
@@ -168,8 +37,7 @@ class ThreeCanvas extends React.Component {
     // ------------------------------------------------------------------------------
     componentDidMount() {
         console.log("componentDidMount");
-        this.loadTextures();
-        window.addEventListener("resize", this.updateDimensions);
+        this.setupScene();
     }
 
     componentWillUnmount() {
@@ -178,32 +46,15 @@ class ThreeCanvas extends React.Component {
         if (this.renderer) {
             this.mount.removeChild(this.renderer.domElement);
         }
-        window.removeEventListener("resize", this.updateDimensions);
     }
 
     render() {
         return (
             <div
-                style={{ width: "100vw", height: "100vw" }}
                 ref={ref => (this.mount = ref)}
             />
         );
     }
-
-    /*
-        render() {
-            return (
-                <div>
-                    <Joystick onChange={this.handleJoystickChange} />
-                    <div
-                        style={{ width: "100vw", height: "75vw" }}
-                        ref={ref => (this.mount = ref)}
-                    />
-                </div>
-            );
-        }
-    */
-
 }
 
-export default ThreeCanvas;
+export default Scene;
